404268a6d92bdbc096701421b96b442def5543bb
ahahaaaa123 <huyd99531@gmail.com>
2024-11-18 22:49:50 +0700

    tangbeiu config.js

--- config.js
+++ config.js
@@ -1,20 +1,21 @@
 const CONFIG = {
-    titleWeb: "Gửi crush của tớ",
-    introTitle: 'Crush yêu dấu',
+    titleWeb: "Gửi cục dàng của a:3",
+    introTitle: 'Cục dàng à=>>',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Thoát web là chấp nhận yêu tớ rồi nhé!!!',
+    title: 'Mai lên giường nhà a chơi một ít trò chơi 2 người nhé 🥰 <3',
+    desc: 'Thoát web là chấp nhận rồi nhé!!!',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại chỉ chơi trò đó vời mình vại <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Chơi thì chơi mà không chơi thì chơi <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay, mình qua đón đi chơi nha.',
+    messDesc: 'Mai mình chở về nhà nhá.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://facebook.com/dinhkaito' //link mess của các bạn. VD: /dinhkaito
+    messLink: 'C:\Program Files\Google\Chrome\Applicationpplication'
+
 }
